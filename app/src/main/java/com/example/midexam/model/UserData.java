8021e4f0abff62af45a9441758ae6c79a201f92d
PPPoria <1316543327@qq.com>
2024-07-21 16:29:19 +0800

    写了个人特面的框架； 写了登录页面

---
+++ app/src/main/java/com/example/midexam/model/UserData.java
@@ -0,0 +1,15 @@
+package com.example.midexam.model;
+
+import java.util.List;
+
+public class UserData {
+    private String name;
+    private String intervalTime;
+    private int waterTarget;
+    private int waterDrink;
+    private int waterSum;
+    private List<String> waterPerDay;
+    private List<String> waterPerMonth;
+    private List<String> jobs;
+    private int focusSum;
+}
---
+++ app/src/main/java/com/example/midexam/presenter/UserPresenter.java
@@ -0,0 +1,4 @@
+package com.example.midexam.presenter;
+
+public class UserPresenter {
+}
