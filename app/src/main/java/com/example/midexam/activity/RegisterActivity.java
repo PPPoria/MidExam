ce2a943a3d426db5239d6564636d3db0bd5fd2c2
PPPoria <1316543327@qq.com>
2024-07-21 21:50:04 +0800

    添加了的注册的页面以及网络请求业务逻辑

--- app/src/main/java/com/example/midexam/activity/RegisterActivity.java
+++ app/src/main/java/com/example/midexam/activity/RegisterActivity.java
@@ -1,7 +1,6 @@
 package com.example.midexam.activity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -18,73 +17,74 @@ import com.example.midexam.R;
 import com.example.midexam.presenter.UserPresenter;
 import com.google.android.material.textfield.TextInputEditText;
 
-import java.io.IOException;
 import java.util.Objects;
 
-public class LogActivity extends AppCompatActivity implements UserDataShowInterface {
-    private static final String TAG = "LogActivity";
+public class RegisterActivity extends AppCompatActivity implements UserDataShowInterface {
+    private static final String TAG = "RegisterActivity";
 
     private TextInputEditText account;
     private TextInputEditText password;
-    private Button logButton;
-    private TextView toRegister;
+    private TextInputEditText passwordAgain;
+    private Button registerButton;
 
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_log);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         getWindow().setNavigationBarColor(R.color.green);
-
         initView();
         initListener();
     }
 
     private void initListener() {
-        logButton.setOnClickListener(v -> {
+        registerButton.setOnClickListener(v -> {
             String accountStr = Objects.requireNonNull(account.getText()).toString();
             String passwordStr = Objects.requireNonNull(password.getText()).toString();
-            UserPresenter.getInstance(this).requestLog(accountStr, passwordStr);
-        });
-        toRegister.setOnClickListener(v -> {
-            startActivity(new Intent(this, RegisterActivity.class));
+            String passwordAgainStr = Objects.requireNonNull(passwordAgain.getText()).toString();
+            UserPresenter.getInstance(this).requestRegister(accountStr, passwordStr, passwordAgainStr);
         });
     }
 
     private void initView() {
-        account = findViewById(R.id.log_account);
-        password = findViewById(R.id.log_password);
-        logButton = findViewById(R.id.log_button);
-        toRegister = findViewById(R.id.toRegister);
+        account = findViewById(R.id.register_account);
+        password = findViewById(R.id.register_password);
+        passwordAgain = findViewById(R.id.register_password_again);
+        registerButton = findViewById(R.id.register_button);
     }
 
     @Override
     public void logSuccess(int STATUS) {
-        finish();
+
     }
 
     @Override
     public void logFail(int STATUS) {
-        if (STATUS == UserPresenter.STATUS_PASSWORD_INCORRECT)
-            Toast.makeText(this, "账号或密码错误", Toast.LENGTH_SHORT).show();
-        else if (STATUS == UserPresenter.STATUS_NO_INTERNET)
-            Toast.makeText(this, "网络错误，请稍后重试", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void registerSuccess(int STATUS) {
-
+        Log.d(TAG, "registerSuccess");
+        Toast.makeText(this, "注册成功", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void registerFail(int STATUS) {
-
+        Log.d(TAG, "registerFail: STATUS = " + STATUS);
+        if (STATUS == UserPresenter.STATUS_NO_INTERNET)
+            Toast.makeText(this, "网络错误，请稍后重试", Toast.LENGTH_SHORT).show();
+        else if (STATUS == UserPresenter.STATUS_ACCOUNT_ALREADY_EXIST)
+            Toast.makeText(this, "账号已存在", Toast.LENGTH_SHORT).show();
+        else if (STATUS == UserPresenter.STATUS_PASSWORDS_INCONSISTENT)
+            Toast.makeText(this, "两次输入的密码不一致", Toast.LENGTH_SHORT).show();
+        else if (STATUS == UserPresenter.STATUS_ACCOUNT_OR_PASSWORD_NOT_SATISFIABLE)
+            Toast.makeText(this, "账号或密码必须大于八位", Toast.LENGTH_SHORT).show();
     }
-
 }
