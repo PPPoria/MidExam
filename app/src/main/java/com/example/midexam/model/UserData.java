7262e39632e51b8da32ee66cec649a4e1835ac3b
PPPoria <1316543327@qq.com>
2024-07-21 17:23:17 +0800

    添加了登录的网络请求业务逻辑

--- app/src/main/java/com/example/midexam/model/UserData.java
+++ app/src/main/java/com/example/midexam/model/UserData.java
@@ -3,7 +3,10 @@ package com.example.midexam.model;
 import java.util.List;
 
 public class UserData {
+    private String msg;
     private String name;
+    private String headImage;
+    private String backgroundImage;
     private String intervalTime;
     private int waterTarget;
     private int waterDrink;
@@ -12,4 +15,100 @@ public class UserData {
     private List<String> waterPerMonth;
     private List<String> jobs;
     private int focusSum;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeadImage() {
+        return headImage;
+    }
+
+    public void setHeadImage(String headImage) {
+        this.headImage = headImage;
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(String backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public String getIntervalTime() {
+        return intervalTime;
+    }
+
+    public void setIntervalTime(String intervalTime) {
+        this.intervalTime = intervalTime;
+    }
+
+    public int getWaterTarget() {
+        return waterTarget;
+    }
+
+    public void setWaterTarget(int waterTarget) {
+        this.waterTarget = waterTarget;
+    }
+
+    public int getWaterDrink() {
+        return waterDrink;
+    }
+
+    public void setWaterDrink(int waterDrink) {
+        this.waterDrink = waterDrink;
+    }
+
+    public int getWaterSum() {
+        return waterSum;
+    }
+
+    public void setWaterSum(int waterSum) {
+        this.waterSum = waterSum;
+    }
+
+    public List<String> getWaterPerDay() {
+        return waterPerDay;
+    }
+
+    public void setWaterPerDay(List<String> waterPerDay) {
+        this.waterPerDay = waterPerDay;
+    }
+
+    public List<String> getWaterPerMonth() {
+        return waterPerMonth;
+    }
+
+    public void setWaterPerMonth(List<String> waterPerMonth) {
+        this.waterPerMonth = waterPerMonth;
+    }
+
+    public List<String> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<String> jobs) {
+        this.jobs = jobs;
+    }
+
+    public int getFocusSum() {
+        return focusSum;
+    }
+
+    public void setFocusSum(int focusSum) {
+        this.focusSum = focusSum;
+    }
 }
--- app/src/main/java/com/example/midexam/presenter/UserPresenter.java
+++ app/src/main/java/com/example/midexam/presenter/UserPresenter.java
@@ -1,4 +1,50 @@
 package com.example.midexam.presenter;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.midexam.helper.Api;
+import com.example.midexam.model.UserData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class UserPresenter {
+    public UserData userData;
+    public Context context;
+    public static UserPresenter presenter = new UserPresenter();
+
+    public static UserPresenter getInstance(Context context) {
+        presenter.context = context;
+        return presenter;
+    }
+
+    //没有获取成功就返回false
+    public boolean getLogCallback(String account, String password) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.uomg.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Api api = retrofit.create(Api.class);
+
+        Call<UserData> dataCall = api.getUserData("log", account, password);
+
+        dataCall.enqueue(new Callback<UserData>() {
+            @Override
+            public void onResponse(Call<UserData> call, Response<UserData> response) {
+                userData = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<UserData> call, Throwable throwable) {
+
+            }
+        });
+
+        return !userData.getMsg().equals("0");
+    }
 }
